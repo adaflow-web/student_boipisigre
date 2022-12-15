568dcad00477403f37c762c6993d562a7ff7e69b
Boipisigre <boipi.sigre@protonmail.com>
2022-12-15 18:03:50 +0100

    houpsss not save

--- TCC/code/run-python/mixing.py
+++ TCC/code/run-python/mixing.py
@@ -1,6 +1,25 @@
-var anumber = "3"
-var astring = " Hello"
 
-var= esult= anumber  + astring
+a_number = "3"
+a_string = " Hello"
 
-console.log(result)
+result= a_number  + a_string
+
+print(result)
+
+number_one = 3
+number_two = 2
+result = number_one + number_two
+
+a_string = " Hello"
+
+message = "result" + a_string
+print(message)
+
+message = str(result) + a_string
+print(message)
+
+apples = "3"
+oranges = 2
+
+result = int(apples) + oranges
+print(result)
