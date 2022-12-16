b99611a52ab8601824043456e934352f76951925
Boipisigre <boipi.sigre@protonmail.com>
2022-12-16 12:41:34 +0100

    tantque logique

--- TCC/code/run-python/thesecret.py
+++ TCC/code/run-python/thesecret.py
@@ -1,22 +1,23 @@
 lesecret=("Pierre")
 print("vous avez droit à 5 essais")
 i=1
-
-while i < 6 :
+test = True
+while test  :
 
     entré = input("Un prénom bien connu ")
 
     if (lesecret == entré) :
         print ("Bravo")
         #break
-        i=6
+        test=False
     else :
         print("Désolé ce n'est pas  " + entré)
         print("essai " + str(i) )
     i=i+1
 
-if i == 6 :
-    print("relancer le programme")
+    if i == 6 :
+        print("relancer le programme")
+        test = False
 
 if i < 6 or i> 6 :
     print("c'est fini")
