b6396dc854b439d2727ee06caec999557f8b5044
Boipisigre <boipi.sigre@protonmail.com>
2022-12-16 09:07:54 +0100

    Le mote secret

---
+++ TCC/code/run-python/secret.py
@@ -0,0 +1,9 @@
+lesecret=("Pierre")
+entré = input("Un prénom bien connu ")
+print
+
+if (lesecret == entré) :
+    print ("Bravo")
+else :
+    print("désollé ce n'est pas  " + entré)
+    print("un autre essai ?")
