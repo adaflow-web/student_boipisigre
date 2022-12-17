5f23057088d81d170adb787417a5077c3197c689
Boipisigre <boipi.sigre@protonmail.com>
2022-12-17 18:14:02 +0100

    Correction de syntaxe et de logique

--- TCC/code/run-python/calculator.py
+++ TCC/code/run-python/calculator.py
@@ -7,8 +7,8 @@ def subtract(a, b):
 num_one = input("Enter a number: ")
 num_two = input("Enter another number: ")
 
-message = "The result of " + num_one + " + " num_two + " is " + add(num_one, num_two)
+message = "The result of " + num_one + " + " + num_two + " is " + str(add(int(num_one), int(num_two)))
 print(message)
 
-message = "The result of " + num_two + " - " + num_one + " is " + subtract(num_one, num_two)
+message = "The result of " + num_one + " - " + num_two + " is " + str(subtract(int(num_one), int(num_two)))
 print(message)
