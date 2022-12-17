78083e4d955050ebfcc511c0c31390902dcf984f
Boipisigre <boipi.sigre@protonmail.com>
2022-12-17 17:19:10 +0100

    trace

---
+++ TCC/code/js-example/imp-err.js
@@ -0,0 +1,16 @@
+
+var a = 0;
+console.log(a); // log
+a = a + 10;
+// at this point, a == 10
+console.log(a); // log
+a = a * 2 + 1;
+// at this point, a == 21
+console.log(a); // log
+a = a + 3 / 2;
+// at this point, a == 12
+console.log(a); // log
+a = a - 11;
+// at this point, a == 1
+
+console.log(a);
