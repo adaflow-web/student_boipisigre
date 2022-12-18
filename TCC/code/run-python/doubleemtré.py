bd0404ca647d326c6fbea3ad01d775b7daaf0f6c
Boipisigre <boipi.sigre@protonmail.com>
2022-12-18 11:09:55 +0100

    création tableau double entré

---
+++ TCC/code/run-python/doubleemtré.py
@@ -0,0 +1,10 @@
+
+employes= [ ["Rick", "Grimes", 38, 180, False],
+            ["Daryl", "Dixon", 43, 189, True],
+            ["Michonne", "Anthony", 32, False]
+        ]
+
+print(employes[0][0])
+print(employes[0])
+print(employes[1][0])
+print(employes[2][0])
