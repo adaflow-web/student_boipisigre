433df0a04a28d3d8393ac51662bed3b7e7a8d146
Boipisigre <boipi.sigre@protonmail.com>
2022-12-18 10:33:04 +0100

    boucle for exemple 2

--- TCC/code/run-python/favsong.py
+++ TCC/code/run-python/favsong.py
@@ -1,5 +1,10 @@
 
 Songs=["les vieux","Brave Margot", "Marseille", "Léa"]
 
+print("Boucle 1")
+for i in range(4):
+    print("chanson " + str(i+1)+" " + Songs[i])
+
+print("Boucle 2")
 for chanson in Songs:
     print(chanson)
