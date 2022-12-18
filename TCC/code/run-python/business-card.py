6e26b8e8ad6cafefb0c29e526cbc4b3fbc5764a0
Boipisigre <boipi.sigre@protonmail.com>
2022-12-18 16:30:21 +0100

    Iniial code

---
+++ TCC/code/run-python/business-card.py
@@ -0,0 +1,9 @@
+card = {"prenom": "pierre",
+        "nom": "loiz",
+        "annais": 1960,
+        "phone": "0765752873",
+        "city": "Lyon",
+        "email": "pierre@proton.mail"
+}
+
+print("nom :" + card[nom]+" Tel: "+card[phone])
