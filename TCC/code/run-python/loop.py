273cc7be94b0def34031f917adb9a15c0f970c65
Boipisigre <boipi.sigre@protonmail.com>
2022-12-18 12:23:58 +0100

    Looping :-)

---
+++ TCC/code/run-python/loop.py
@@ -0,0 +1,11 @@
+# version for
+print("\nVersion for\n")
+for i in range(6):
+    print(str(i) + " is un nombre")
+
+# version while condition:
+print("\nversion while\n")
+i=0
+while i<10:
+    print(str(i) + " is un nombre")
+    i=i+1
