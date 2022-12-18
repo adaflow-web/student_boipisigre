6a092991a09c4e9d2684d88e389e3ad68af57b78
Boipisigre <boipi.sigre@protonmail.com>
2022-12-18 11:02:42 +0100

    boucle en fortran IV ;-)

--- TCC/code/run-python/favsong.py
+++ TCC/code/run-python/favsong.py
@@ -1,4 +1,6 @@
 
+# from goto import goto
+
 Songs=["les vieux","Brave Margot", "Marseille", "Léa"]
 
 print("Boucle 1")
@@ -14,3 +16,13 @@ i=0
 while (i < 4) :
     print("chanson " + str(i+1)+" " + Songs[i])
     i=i+1
+    
+"""
+print("boucle en mode Fortran IV")
+i=0
+#("hi")
+if (i < 4):
+    print("chanson " + str(i+1)+" " + Songs[i])
+    i=i+1
+    goto("hi")
+"""
