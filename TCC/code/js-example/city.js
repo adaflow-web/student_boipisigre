e72de56a7514abe6627bccf35deaf6cab7841a26
Boipisigre <boipi.sigre@protonmail.com>
2022-12-18 16:43:55 +0100

    code initital

---
+++ TCC/code/js-example/city.js
@@ -0,0 +1,14 @@
+/* var city = {name: "Nyon", country: "Switzerland", population: 11000}
+console.log(city.country);
+console.log(city["name"]);
+*/
+
+var card = {prenom: "pierre",
+        nom: "loiz",
+        annais: 1960,
+        phone: "0765752873",
+        city: "Lyon",
+        email: "pierre@proton.mail"
+}
+
+console.log("nom :" + card["nom"]+" Tel: "+card["phone"])
