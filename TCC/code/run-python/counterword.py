63b651fbb8d60c8fca96fb40ad9b7a0cf82fe8f1
Boipisigre <boipi.sigre@protonmail.com>
2022-12-21 16:14:48 +0100

    version 2

--- TCC/code/run-python/counterword.py
+++ TCC/code/run-python/counterword.py
@@ -13,3 +13,18 @@ print("nombre de mots " + str(nbmots) )
 
 
 text_file.close()
+
+# partwo
+lefichier= open("zen.txt")
+content = lefichier.read()
+lefichier.close()
+
+words = content.replace( "-" ,"").replace( "*", "").replace(",", "").replace(".", "").replace("!", "").replace("'", "").replace("\n", " ").replace("  ", " ").strip()
+
+words_count = len(words.split(" "))
+print(words_count)
+
+newtexte = content.replace("is", "should be")
+
+
+print(newtexte.upper())
