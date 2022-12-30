c62ba2571213ad29561ef7a8c3982963d063ba86
Boipisigre <boipi.sigre@protonmail.com>
2022-12-30 09:59:06 +0100

    Ajout fonctions

--- TCC/code/run-python/noteclass.py
+++ TCC/code/run-python/noteclass.py
@@ -23,6 +23,17 @@ class Note(object):
     createur = ""
     texte = ""
 
+    def printtext(self):
+        return f"{self.texte}"
+
+    def printcreateur(self):
+        return f"{self.createur}"
+
+    def printnum(self):
+        return f"{self.numero}"
+
+
+
 note1 = Note()
 
 note1.numero = 1
@@ -38,5 +49,7 @@ note2.modification = "30.12.2022"
 note2.createur = "Paul"
 note2.texte = "voici ma seconde note"
 
-print ("la note "+ str(note1.numero)+ " : " + note1.texte + " a été créée par " + note1.createur)
-print ("la note "+ str(note2.numero)+ " : "  + note2.texte + " a été créée par " + note2.createur)
+print ("la note "+ str(note1.numero)+ " : " + note1.texte + " elle a été créée par " + note1.createur)
+print ("la note "+ str(note2.numero)+ " : "  + note2.texte + " elle a été créée par " + note2.createur)
+
+print("la note " + note1.printnum() +" : "+ note1.printtext()+ " Elle a été créée par "+ note1.printcreateur())
