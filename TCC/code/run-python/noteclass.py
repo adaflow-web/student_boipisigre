afcffca8331361555b59035450dd66d3f0573dd6
Boipisigre <boipi.sigre@protonmail.com>
2022-12-30 09:14:31 +0100

    code première classe les date ne sont pas des date mais des strings

---
+++ TCC/code/run-python/noteclass.py
@@ -0,0 +1,42 @@
+
+"""
+pour avoir des dates.
+
+import datetime
+
+x = datetime.datetime(2020, 5, 17)
+"""
+
+
+class Note(object):
+    """
+    creation d'une classe note  comprenant
+        un numero
+        une date de création
+        une date de modification
+        nom du createur
+        le texte de la note
+    """
+    numero = 0
+    creation = "01.01.1900"
+    modification = "02.01.1900"
+    createur = ""
+    texte = ""
+
+note1 = Note()
+
+note1.numero = 1
+note1.creation = "30.12.2022"
+note1.modification = "30.12.2022"
+note1.createur = "Pierre"
+note1.texte = "voici ma premiere note"
+
+note2 = Note()
+note2.numero = 2
+note2.creation = "30.12.2022"
+note2.modification = "30.12.2022"
+note2.createur = "Paul"
+note2.texte = "voici ma seconde note"
+
+print ("la note "+ str(note1.numero)+ " : " + note1.texte + " a été créée par " + note1.createur)
+print ("la note "+ str(note2.numero)+ " : "  + note2.texte + " a été créée par " + note2.createur)
