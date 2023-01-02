fba09670bc6f83f3575f1583d9dc680e6b7aeec7
Boipisigre <boipi.sigre@protonmail.com>
2023-01-02 14:35:06 +0100

    ajout suppression premier et dernier texte

--- TCC/code/interaction-js/interaction.js
+++ TCC/code/interaction-js/interaction.js
@@ -10,8 +10,9 @@ const Saisie2 = document.getElementById('Saisie2') ;
 
 // le button
 const Action1 = document.getElementById("bu1") ;
-
 const Action2 = document.getElementById("bu2") ;
+const Action3 = document.getElementById("bu3") ;
+const Action4 = document.getElementById("bu4") ;
 
 function remplir(){
   Letexte.innerHTML = Saisie1.value+"<br>";
@@ -27,6 +28,25 @@ function addtext(){
   Saisie2.value ='le texte NOUVEAU';
 }
 
+function remove1(){
+
+  const textes = document.getElementsByClassName('deco') ;
+
+  if (textes.length > 0 ){
+    Lajout.removeChild(textes[0])
+  }
+}
+
+function remove2(){
+
+  const textes = document.getElementsByClassName('deco') ;
+
+  if (textes.length > 0 ){
+    var position = textes.length - 1;
+    Lajout.removeChild(textes[position]);
+  }
+}
+
 Action1.addEventListener("click", remplir);
 Action1.addEventListener("dblclick", function(){
   document.getElementById("Letexte").innerHTML = "........";
@@ -34,3 +54,7 @@ Action1.addEventListener("dblclick", function(){
 )
 
 Action2.addEventListener("click", addtext);
+
+Action3.addEventListener("click", remove1);
+
+Action4.addEventListener("click", remove2);
