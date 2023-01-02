f10a3848ab1d22e69e1eb15b43dc23dab562afc8
Boipisigre <boipi.sigre@protonmail.com>
2023-01-02 11:48:37 +0100

    pb avec Css ...

--- TCC/code/interaction-js/interaction.js
+++ TCC/code/interaction-js/interaction.js
@@ -2,20 +2,35 @@
 
 // le texte
 const Letexte = document.getElementById('Letexte') ;
+const Lajout = document.getElementById('ajout');
 
 // la saisie
-const Saisie = document.getElementById('Saisie') ;
+const Saisie1 = document.getElementById('Saisie1') ;
+const Saisie2 = document.getElementById('Saisie2') ;
 
 // le button
-const Action = document.getElementById("bu") ;
+const Action1 = document.getElementById("bu1") ;
+
+const Action2 = document.getElementById("bu2") ;
 
 function remplir(){
-  Letexte.innerHTML = Saisie.value+"<br>";
-  Saisie.value ='un autre texte'
+  Letexte.innerHTML = Saisie1.value+"<br>";
+  Saisie1.value ='changer le texte'
 }
 
-Action.addEventListener("click", remplir);
-Action.addEventListener("dblclick", function(){
+function addtext(){
+  const Newajout=document.createElement("p") ;
+  Newajout.innerHTML = Saisie2.value +"<br>";
+  Newajout.className="deco";
+  
+  Lajout.appendChild(Newajout);
+  Saisie2.value ='le texte NOUVEAU';
+}
+
+Action1.addEventListener("click", remplir);
+Action1.addEventListener("dblclick", function(){
   document.getElementById("Letexte").innerHTML = "........";
 }
 )
+
+Action2.addEventListener("click", addtext);
