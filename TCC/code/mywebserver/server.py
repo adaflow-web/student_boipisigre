cf3eb909fd9c188a8450196b43523c47561d3f7b
Boipisigre <boipi.sigre@protonmail.com>
2023-01-03 17:01:40 +0100

    mon server.py ;-)

--- TCC/code/mywebserver/server.py
+++ TCC/code/mywebserver/server.py
@@ -1,1 +1,11 @@
 # ls version simple
+
+import http.server
+import socketserver
+
+port = 80
+Handler = http.server.SimpleHTTPRequestHandler
+
+httpd = socketserver.TCPServer(("", 80 ), Handler)
+
+httpd.serve_forever()
