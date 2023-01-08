21921fc5d936a8f75f4221b72d8c7940c8415b1b
Boipisigre <boipi.sigre@protonmail.com>
2023-01-08 11:01:33 +0100

    Modification de menu Test de inclusion html :-( css sur note

--- TCC/code/flaskproject/notes/notes.py
+++ TCC/code/flaskproject/notes/notes.py
@@ -22,6 +22,13 @@ def add_notes(texte):
     notesdb.close()
     return
 
+def affiche_note(listenotes):
+    change_value=""
+    for note in listenotes :
+        champs=note.split("€")
+        change_value=change_value + "<p class='titre'>" + champs[0] + "</p> <p class='corps'>" + champs[1] + "</p>"
+    return change_value
+
 @app.route("/")
 def homepage():
     return get_html("index")
@@ -34,15 +41,16 @@ def about():
 def notes():
     notepage = get_html("notes")
     lesnotes=get_notes()
-    change_value=""
-    for note in lesnotes :
-        champs=note.split("€")
-        change_value=change_value + "<p>" + champs[0] + "</p> <p>" + champs[1] + "</p>"
-    return notepage.replace("$$MesNotes$$",change_value)
+    return notepage.replace("$$MesNotes$$",affiche_note(lesnotes))
 
 @app.route("/addnotes")
 def addnotes():
     return get_html("ajoutnote")
+
+app.route("/menu")
+def menu():
+    return get_html("menu")
+
 
 @app.route("/ajoutnote")
 def ajoutnote():
@@ -73,7 +81,7 @@ def rechercher():
         for unenote in lesnotes:
             champs=unenote.split("€")
             if (txt_recherche.upper() in champs[0].upper()):
-                change_value=change_value + "<p>" + champs[0] + "</p> <p>" + champs[1] + "</p>"
+                change_value=change_value + "<p class='titre'>" + champs[0] + "</p> <p class='corps'>" + champs[1] + "</p>"
                 trouvé=True
 
         if not(trouvé):
